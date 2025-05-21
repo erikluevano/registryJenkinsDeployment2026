2d8be3e882863ef4019cbf6acbb636bc2244059f
Alejandro Mauricio González <amgdark@uaz.edu.mx>
2025-05-21 17:36:14 -0600

    Corrige prueba para el mensaje de bienvenida

--- pruebas_aceptacion/features/steps/login.py
+++ pruebas_aceptacion/features/steps/login.py
@@ -2,12 +2,10 @@ from behave import when, given, then
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-
 @given(u'que ingreso a la url: "{url}"')
 def step_impl(context, url):
     # context.driver = webdriver.Chrome()
     context.driver.get(url)
-
 
 @given(u'capturo el usuario: "{username}" y la contraseña "{password}"')
 def step_impl(context, username, password):
@@ -20,16 +18,16 @@ def step_impl(context):
     xpath = '//*[@id="login-form"]/div[3]/input'
     context.driver.find_element(By.XPATH, xpath).click()
 
-
 @then(u'puedo ver el mensaje "{mensaje}"')
 def step_impl(context, mensaje):
-    div_mensaje = context.driver.find_element(By.ID, 'user-tools')
+    div_mensaje = context.driver.find_element(By.ID, 'bienvenida')
     assert mensaje in div_mensaje.text, \
         f"El mensaje {mensaje} no se encuentra en {div_mensaje.text}"
-
+    
 
 @then(u'puedo ver el mensaje de error "{mensaje}"')
 def step_impl(context, mensaje):
     div_mensaje = context.driver.find_element(By.CLASS_NAME, 'errornote')
     assert mensaje in div_mensaje.text, \
         f"El mensaje {mensaje} no se encuentra en {div_mensaje.text}"
+
